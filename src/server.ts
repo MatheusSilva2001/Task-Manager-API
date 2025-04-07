6eb9d22aac84c1d20e83bc85b54bccc63393d57f
Matheus Rian <silva.matheus200107@gmail.com>
2025-04-07 19:24:26 -0300

    update whitelist

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,7 @@ app.use(cookieParser());
 const whiteList = [
   "http://localhost:5173",
   "http://127.0.0.1:5173",
-  "https://task-manager-wdc.vercel.app",
+  "https://task-manager-api-nccv.onrender.com",
 ];
 
 app.use(
