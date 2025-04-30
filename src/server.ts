bbe1c434cac4e27348b9b0514a6979d8fe5da64f
Matheus Rian <silva.matheus200107@gmail.com>
2025-04-30 20:11:16 -0300

    add front link to white list

--- src/server.ts
+++ src/server.ts
@@ -15,9 +15,9 @@ app.use(express.json());
 app.use(cookieParser());
 
 const whiteList = [
-  "http://localhost:5173",
-  "http://127.0.0.1:5173",
-  "https://task-manager-api-nccv.onrender.com",
+  // "http://localhost:5173",
+  // "http://127.0.0.1:5173",
+  "task-manager-wdc-f3wa.vercel.app",
 ];
 
 app.use(
